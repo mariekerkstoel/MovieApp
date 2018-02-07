7c407f7e3a14ea1e78dcaf535464cc3a993183e6
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-02-07 14:29:08 +0000

    Set results from api call to state

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,18 +4,19 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: '', searchedMovies: []};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   fetchMovieData(title) {
+    let self = this
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=121486b23802e0b6735125ff1892f340&language=en-US&query=${title}&page=1&include_adult=false`)
       .then(function(response){
         return response.json()
       })
       .then(function(jsonData){
-        console.log(jsonData.results)
+        self.setState({searchedMovies: jsonData.results})
       });
   }
 
