c1562e40d24568a36792f924cb146f21d0053c61
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-02-08 17:06:04 +0000

    On click, save favorite movie to mongo db

--- BackEnd/routes/api.js
+++ BackEnd/routes/api.js
@@ -1,13 +1,18 @@
 const express = require('express')
-
 const router = express.Router()
+const db = require('../config/dbUtils').db
 
 router.get('/', (req, res) =>{
   res.send('Hello, you are on the api route')
 })
 
 router.post('/favorites', (req, res) => {
-  console.log(req.body)
+  const collection = db.get('favorites')
+
+  collection.insert(req.body)
+   .then((docs) => {
+   }).catch((err) => {
+   })
   res.send(200)
 })
 
