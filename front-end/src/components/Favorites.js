15197af949655cf5445cd4695c5729001268f465
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-02-09 14:30:07 +0000

    Add favorites array to state, add componentdidmount function that fetches from backend api, list component renders favorites

--- front-end/src/components/Favorites.js
+++ front-end/src/components/Favorites.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import List from './List';
 
 class Favorites extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {favorites: []};
+  }
+
+  componentDidMount(){
+    let self = this
+    fetch('http://localhost:5000/api/favorites')
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(jsonData){
+      self.setState({favorites: jsonData})
+      console.log(self.state.favorites)
+    })
+  }
+
   render(){
     return(
-      <h1>He</h1>
+      <List movies={this.state.favorites} />
     )
   }
 }
